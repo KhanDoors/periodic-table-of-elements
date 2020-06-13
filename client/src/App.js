9d52f0b492360fc46332d0bd06de18e7fc1901a9
syyad <syyadkhan@yahoo.com>
2020-06-13 17:01:31 -0400

    setup done

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PeriodicTable from "./components/PeriodicTable";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PeriodicTable />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/PeriodicTable.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PeriodicTable.css";
+
+const PeriodicTable = () => {
+  return (
+    <div>
+      <h1>Periodic table</h1>
+    </div>
+  );
+};
+
+export default PeriodicTable;
